80bcf2d6f65c860cfb467bcf2824599893fc22c4
Tsvetan40 <tsvetan.gabrovski@abv.bg>
2023-01-27 13:38:27 +0200

    registration form validation

--- registration/registration.js
+++ registration/registration.js
@@ -1,5 +1,6 @@
 import {User} from "../User.js"
 
+const form = document.getElementById('form')
 const register = document.getElementById('registration')
 const email = document.getElementById('email')
 const password = document.getElementById('password')
@@ -27,10 +28,27 @@ email.addEventListener('keydown', () => {
     }
 })
 
-register.addEventListener('click', () => {
+form.addEventListener('submit', (e) => {
     
-    if (email.value == null || password.value == null) {
-        return
+    debugger
+    if (email.value == "" || password.value == "") {
+        e.preventDefault()
     }
 
+    const divError = document.getElementsByClassName('error-hidden')[0]
+    
+    if (localStorage.getItem(email.value) != null) {
+        divError.classList.add('error')
+
+        e.preventDefault()
+    
+    } else {
+        if (divError.classList.contains('error')) {
+            divError.classList.remove('error')
+        }
+    }
+    
+
+    window.localStorage.setItem(email.value, new User(email.value, password.value))
+    return true
 })
