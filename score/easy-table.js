52fd96d3f3c9f26b325192077762f57f52b79d27
Tsvetan40 <tsvetan.gabrovski@abv.bg>
2023-02-05 15:59:55 +0200

    style for easy-table

---
+++ score/easy-table.js
@@ -0,0 +1,48 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const tbody = document.getElementsByTagName('tbody')[0]
+
+    const results = JSON.parse(window.localStorage.getItem('easy_table'))
+
+    const sorted = results.sort((a, b) => {
+        return toSeconds(a) - toSeconds(b)
+    })
+
+    sorted.forEach((result, index) => {
+        const tr = document.createElement('tr')
+        const tdPosition = document.createElement('td')
+        const tdEmail = document.createElement('td')
+        const tdTime = document.createElement('td')
+        
+        
+        tdPosition.textContent = `${index}`
+        tdPosition.classList.add('tbody-item')
+        
+        tdEmail.textContent = `${result['email']}`
+        tdEmail.classList.add('tbody-item')
+        
+        tdTime.textContent = `${result['result']}`
+        tdTime.classList.add('tbody-item')
+
+        tr.appendChild(tdPosition)
+        tr.appendChild(tdEmail)
+        tr.appendChild(tdTime)
+        tbody.appendChild(tr)
+    })
+
+
+    function toSeconds(a) {
+       
+        const result = a['result']
+        const trimmed = result.replace(/\s/g, '')
+        const resultValues = trimmed.split(':')
+
+        const minutes = resultValues[0]
+        const seconds = resultValues[1]
+
+        if (minutes[0] == '0') {
+            return minutes[1] * 60 + seconds
+        }
+        
+        return minutes * 60 + seconds
+    }
+})
